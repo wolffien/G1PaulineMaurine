84e2838fa908160b5c097478d3bfcc9478489961
Maurine Cros <maurine.cros@mail.com>
2021-11-24 15:41:15 +0100

    début to do list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+// import './components/header.css';
+
 
 export default function App() {
     const [todos, setTodos] = useState([
@@ -12,14 +15,15 @@ export default function App() {
     return (
         // cette view comprends toute les autres view
         <View style={styles.container}>
-            {/* view qui prends en compte le header */}
+            {/* appeler le component header qui a été crée dans ./components/header.js */}
+            <Header />
             <View style={styles.content}>
                 {/* to form */}
                 <View style={styles.list}>
                     <FlatList
                         data={todos}
                         // cette fonction va retourner les listes
-                        renderItem={( item ) => (
+                        renderItem={({ item }) => (
                             // définir que chaque note est du texte
                             <Text>{item.text}</Text>
                         )}
@@ -34,5 +38,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundcolor: '#fff',
+    },
+    // styliser la view qui s'appelle content (avant la liste)
+    content: {
+        padding: 40,
+    },
+    // styliser la view qui s'appelle list = le form
+    list: {
+        margintTop: 20,
     }
 });
