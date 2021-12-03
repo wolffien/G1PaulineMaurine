859760d262571b3f39bdd033e8ea9c8d98879d84
Maurine Cros <maurine.cros@mail.com>
2021-12-03 11:14:57 +0100

    suppression d'une todo avec jp

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -22,86 +22,112 @@ function TodoList() {
 
         const updatedTodos = [...todos, todo].filter(todo => todo.id !== id);
 
-        setTodos(updatedTodos)
-    };
+        // Suppression d'une todo
+        const removeTodo = id => {
+            const removeArr = [...todos].filter(todo => todo.id !== id);
+
+            setTodos(removeArr);
+        };
+
+        //supression d'une todo avec JP
+
+        //todo[1]=null
+        // ou
+        // todos[1].id=''
+        // todos[1].title=''
+        // todos[1].description=''
+        // et on recopie
+        // todos[1]=todos[2]
+        // todos[2]=todos[3]
+        // 
+        // todos[n]=todos[n+1]
+
+        //     n=1
+        //     // todos[n]=todos[n+1]
+        //     n++
+        //     // todos[n]=todos[n+1]
+        //     n++
+        //     // todos[n]=todos[n+1]
+        //     n++
 
 
-    // Suppression d'une todo
-    const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id);
+        //     for(let n=nuLigneASUp; n<todos.length-1;n++){
+        //         todos[n]=todos[n+1]
+        //     }
 
-        setTodos(removeArr);
-    };
+        //     setTodos(updatedTodos)
+        // };
 
-    // Barre la todo quand on clique sur son texte (signifie qu'elle a été réalisée)
-    const completeTodo = id => { //fonction completeTodo prend en paramètre id 
-        let updatedTodos = todos.map( //avec le map on parcour chaque cellule du tableau todos
-            todo => { // et on fait appel à une fonction anonyme ( pas de nom) qui prend en paramèrtre todo (une cellule du tableau)
-                if (todo.id === id) { // si l'id de la cellule = id passé en paramètre alors
-                    todo.isComplete = !todo.isComplete; // on intervertit l'état de la chose à faire
+
+        // Barre la todo quand on clique sur son texte (signifie qu'elle a été réalisée)
+        const completeTodo = id => { //fonction completeTodo prend en paramètre id 
+            let updatedTodos = todos.map( //avec le map on parcour chaque cellule du tableau todos
+                todo => { // et on fait appel à une fonction anonyme ( pas de nom) qui prend en paramèrtre todo (une cellule du tableau)
+                    if (todo.id === id) { // si l'id de la cellule = id passé en paramètre alors
+                        todo.isComplete = !todo.isComplete; // on intervertit l'état de la chose à faire
+                    }
+                    return todo; // retourne la chose à faire 
                 }
-                return todo; // retourne la chose à faire 
-            }
-        )
+            )
 
-        // ou en plus court JUSTE POUR LE PREMIER
-        // let updatedTodos =  todos.slice()
-        // debugger
-        // updatedTodos[0].isComplete = !updatedTodos[0].isComplete 
-        setTodos(updatedTodos);
-    };
+            // ou en plus court JUSTE POUR LE PREMIER
+            // let updatedTodos =  todos.slice()
+            // debugger
+            // updatedTodos[0].isComplete = !updatedTodos[0].isComplete 
+            setTodos(updatedTodos);
+        };
 
-    // const TetatSaisi = React.useState ('') // je déclare une variable d'état + '' = chaine de caractère vide qui me sert à initialiser ma var d'état
-    // const etatSaisi = TetatSaisi [0] ; //je nomme ma variable d'état etatSaisi
-    // const setetatSaisi = TetatSaisi[1] //je nomme ma fonction de changement d'état
+        // const TetatSaisi = React.useState ('') // je déclare une variable d'état + '' = chaine de caractère vide qui me sert à initialiser ma var d'état
+        // const etatSaisi = TetatSaisi [0] ; //je nomme ma variable d'état etatSaisi
+        // const setetatSaisi = TetatSaisi[1] //je nomme ma fonction de changement d'état
 
-    //ou en plus court
-    const [etatSaisi, setetatSaisi] = React.useState('')
-    const [etatValide, setetatValide] = React.useState('')
+        //ou en plus court
+        const [etatSaisi, setetatSaisi] = React.useState('')
+        const [etatValide, setetatValide] = React.useState('')
 
-    const lorsDuChangement = (event) => {
-        // event donné en param par REACT
-        // target propriété de event qui designe l'INPUT qui a déclenché l'event
-        // value propriété de target qui contient la valeur de l'INPUT
-        // event.target.value = la valeur saisie
-        setetatSaisi(event.target.value)
+        const lorsDuChangement = (event) => {
+            // event donné en param par REACT
+            // target propriété de event qui designe l'INPUT qui a déclenché l'event
+            // value propriété de target qui contient la valeur de l'INPUT
+            // event.target.value = la valeur saisie
+            setetatSaisi(event.target.value)
 
 
+        }
+
+        const valideEtatTheme = () => {
+
+            setetatValide(etatSaisi)
+
+        }
+
+
+        return (
+            <div
+                className={etatValide == "sombre" ? 'sombre' : 'clair'}
+            >
+
+                <p>
+                    <br></br>
+                    <input
+                        className='todo-input'
+                        value={etatSaisi}
+                        onChange={lorsDuChangement}
+                        placeholder='Écrire "clair" ou "sombre"'
+                    ></input>
+                    <button className='todo-button' onClick={valideEtatTheme}>Changer le fond</button>
+                </p>
+                <h1>Bucket List</h1>
+                <TodoForm onSubmit={addTodo} />
+                {/* afficher la todo une fois validé, ainsi que l'option supp et edit */}
+                <Todos
+                    todos={todos}
+                    completeTodo={completeTodo}
+                    removeTodo={removeTodo}
+                    updateTodo={updateTodo}
+                />
+            </div>
+        );
     }
 
-    const valideEtatTheme = () => {
-
-        setetatValide(etatSaisi)
-
-    }
-
-
-    return (
-        <div
-            className={etatValide == "sombre" ? 'sombre' : 'clair'}
-        >
-
-            <p>
-                <br></br>
-                <input
-                    className='todo-input'
-                    value={etatSaisi}
-                    onChange={lorsDuChangement}
-                    placeholder='Écrire "clair" ou "sombre"'
-                ></input>
-                <button className='todo-button' onClick={valideEtatTheme}>Changer le fond</button>
-            </p>
-            <h1>Bucket List</h1>
-            <TodoForm onSubmit={addTodo} />
-            {/* afficher la todo une fois validé, ainsi que l'option supp et edit */}
-            <Todos
-                todos={todos}
-                completeTodo={completeTodo}
-                removeTodo={removeTodo}
-                updateTodo={updateTodo}
-            />
-        </div>
-    );
-}
-
-export default TodoList;
+    export default TodoList;
